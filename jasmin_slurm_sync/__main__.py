f8c9dd79defff5fe962c3e51f8588241c61a80bf
Alex Manning <alexander.manning@stfc.ac.uk>
2024-09-10 13:53:55 +0100

    Add systemd integration.

--- jasmin_slurm_sync/__main__.py
+++ jasmin_slurm_sync/__main__.py
@@ -1,19 +1,25 @@
 import logging
+import os
 import pathlib
 import time
 
 import pydantic_settings
+import sdnotify
 
 from . import cli
 from . import settings as settings_module
 from . import sync
 
+system_notify = sdnotify.SystemdNotifier()
 args = cli.SyncArgParser().parse_args()
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 logger.info("Starting sync of SLURM users.")
+system_notify.notify(f"MAINPID={os.getpid()}")
+system_notify.notify("READY=1")
+
 while True:
     logger.debug("Loading settings.")
     settings = settings_module.load_settings(pathlib.Path(args.config))
@@ -29,4 +35,5 @@ while True:
         time.sleep(settings.daemon_sleep_time)
     else:
         logger.info("Running in one-shot mode. Quitting.")
+        system_notify.notify("STOPPING=1")
         break
