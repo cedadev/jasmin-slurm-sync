909731449dc706e6a026ebdfd601c32f8fe29173
Alex Manning <alexander.manning@stfc.ac.uk>
2024-11-05 16:46:26 +0000

    ().

--- jasmin_slurm_sync/sync/user.py
+++ jasmin_slurm_sync/sync/user.py
@@ -79,7 +79,7 @@ class UserSyncingMixin:
                             self.settings.extra_account_mapping[service_name]
                         )
         # Add the no project account to users who have no other account.
-        for username, accounts in user_accounts.keys:
+        for username, accounts in user_accounts.keys():
             if len(accounts) == 1:
                 user_accounts[username].add(self.settings.no_project_account)
         return user_accounts
